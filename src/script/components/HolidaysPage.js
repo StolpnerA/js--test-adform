6ca04aad18e8fddfe775fb2af41294f62e929ac7
AndreyS <andreysx@tut.by>
2017-09-28 12:40:00 +0300

    add promise in work with data

--- src/script/components/HolidaysPage.js
+++ src/script/components/HolidaysPage.js
@@ -4,26 +4,27 @@ let placeRender = document.querySelector(".workPlace");
 
 class HolidaysPage {
   renderPage() {
-    let employees = db.fetch("employees");
-    let select = `<select class="form-control" id="exampleFormControlSelect1">`;
-    employees.forEach(function(element) {
-      select += `<option>${element.name}</option>`;
-    }, this);
-    placeRender.innerHTML = `
-    <div class="container">
-    <div class="form-group">
-        <label for="exampleFormControlSelect1">Выбор сотрудника</label>
-        ${select}
-        </select>
-        <label for="dateFrom">Дата начало отпуска: </label>
-        <input type="date" name="calendar" class="form-control" id="dateFrom">
-        <label for="dateTo">Дата конца отпуска</label>
-        <input type="date" name="calendar" class="form-control" id="dateTo">
-        <span class="info"></span>
-        <input class="submitHoliday btn btn-outline-primary" type="submit" value="Submit">
-    </div>
-</div>
-      `;
+    db.fetch("employees").then(data => {
+      let select = `<select class="form-control" id="exampleFormControlSelect1">`;
+      data.forEach(function(element) {
+        select += `<option>${element.name}</option>`;
+      }, this);
+      placeRender.innerHTML = `
+      <div class="container">
+      <div class="form-group">
+          <label for="exampleFormControlSelect1">Выбор сотрудника</label>
+          ${select}
+          </select>
+          <label for="dateFrom">Дата начало отпуска: </label>
+          <input type="date" name="calendar" class="form-control" id="dateFrom">
+          <label for="dateTo">Дата конца отпуска</label>
+          <input type="date" name="calendar" class="form-control" id="dateTo">
+          <span class="info"></span>
+          <input class="submitHoliday btn btn-outline-primary" type="submit" value="Submit">
+      </div>
+  </div>
+        `;
+    });
   }
 }
 
