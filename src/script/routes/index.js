544dab45d5545b8bccae3d2fd2143e0e12006305
AndreyS <andreysx@tut.by>
2017-09-28 11:14:39 +0300

    Edite LS structore

--- src/script/routes/index.js
+++ src/script/routes/index.js
@@ -18,27 +18,32 @@ var index = {
         {
           id: count++,
           name: "Столпнер Андрей Сергеевич",
-          position: "Front-end developer"
+          position: "Front-end developer",
+          countDaysHoli: 24
         },
         {
           id: count++,
           name: "Столпнер Сергей Сергеевич",
-          position: "System Administrator"
+          position: "System Administrator",
+          countDaysHoli: 24
         },
         {
           id: count++,
           name: "Сукора Станислав Игоревич",
-          position: "Back-end developer"
+          position: "Back-end developer",
+          countDaysHoli: 24
         },
         {
           id: count++,
           name: "Довгаль Алексей Владимирович",
-          position: "Front-end developer"
+          position: "Front-end developer",
+          countDaysHoli: 24
         },
         {
           id: count++,
           name: "Иванов Иван Иванович",
-          position: "Front-end developer"
+          position: "Front-end developer",
+          countDaysHoli: 24
         }
       ];
       db.setItem("employees", employees);
